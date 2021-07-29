491ba7fb73da008b3b28e29fa624d16a3c9fdc07
haruyama8940 <yaruo0406@gmail.com>
2021-07-29 18:28:07 +0900

    debug code

--- src/joy_cmd.cpp
+++ src/joy_cmd.cpp
@@ -7,36 +7,43 @@
 #include <std_msgs/String.h>
 
 ros::Subscriber joy_sub;
-ros::Publisher joy_cmd;
+ros::Publisher joy_pub;
 //boost::array<int,10>joy_b;
 std::vector<int> joy_b;
-/*
-void Joy_Callback(sensor_msgs::Joy joy_msg)
+sensor_msgs::Joy cmd_topic;
+void Joy_Callback(const sensor_msgs::Joy& joy_msg)
 {
-std::vector<int> joy_b(joy_msg.buttons);
+//std::vector<int> joy_b(joy_msg.buttons);
+//cmd_topic = joy_msg;
+ROS_INFO("Joy_callback");
 std_msgs::String msg;
-  if (joy_b[4]==1)
-  {
-    msg.data="go_straight";
-    joy_cmd.publish(msg.data);
-  }
-  if (joy_b[3]==1)
+
+  if (joy_msg.buttons[1]==1)
   {
     msg.data="turn_right";
-    joy_cmd.publish(msg.data);
+    joy_pub.publish(msg);
+    ROS_INFO("right");
   }
-  if (joy_b[4]==1)
+
+  if (joy_msg.buttons[2]==1)
   {
     msg.data="turn_left";
-    joy_cmd.publish(msg.data);
+    joy_pub.publish(msg);
+    ROS_INFO("left");
   }
-  
+  if (joy_msg.buttons[3]==1)
+  {
+    msg.data="go_straight";
+    joy_pub.publish(msg);
+    ROS_INFO("go");
+  }
 }
-*/
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "joy_cmd");        // ノードの初期化（ノード名を"srv_test"に設定）
   ros::NodeHandle nh;                       // ノードハンドルの生成
-  //joy_sub = nh.subscribe("joy", 10, &Joy_Callback);
+  joy_pub = nh.advertise<std_msgs::String>("joy_cmd", 10);
+  joy_sub = nh.subscribe("joy", 10, Joy_Callback);
   ROS_INFO("muripo");
   ros::spin();                              // リクエストの無限待ち
   return 0;
