2f77f1505e25f304952bbb1ebf25551cdb18b08b
haruyama8940 <yaruo0406@gmail.com>
2021-09-28 14:48:13 +0900

    fix cmd

--- src/joy_cmd.cpp
+++ src/joy_cmd.cpp
@@ -20,54 +20,55 @@ std::vector<std::string> cmd_list = {"go_stright","turn_right","turn_left"};
 //std::array<int,3> data{0,0,0};
 int list[3][3]={
                 {1,0,0},
-                {0,1,0}
-                {0,0,1}
+                {0,1,0},
+                {0,0,1},
                 };
                 
 void Joy_Callback(const sensor_msgs::Joy& joy_msg)
 {
-//std::vector<int> joy_b(joy_msg.buttons);
-//cmd_topic = joy_msg;
-ROS_INFO("Joy_callback");
-std_msgs::String msg;
-cmd_data.cmd_array;
+  //std::vector<int> joy_b(joy_msg.buttons);
+  //cmd_topic = joy_msg;
+  ROS_INFO("Joy_callback");
+  std_msgs::String msg;
+  cmd_data.cmd_array;
+  int i;
   if (joy_msg.buttons[1]==1)
-  {
-    //msg.data="turn_right";
-    cmd_data.cmd_word=cmd_list[1];
-    for ( i = 0; i < 3; i++)
     {
-    cmd_data.cmd_array[i]=list[0][i]
-    }
+      //msg.data="turn_right";
+      cmd_data.cmd_word=cmd_list[1];
+      for ( i = 0; i < 3; i++)
+      {
+      cmd_data.cmd_array[i]=list[1][i];
+      }
     
-    joy_pub.publish(msg);
-    ROS_INFO("right");
-  }
+      joy_pub.publish(msg);
+      ROS_INFO("right");
+    }
 
   if (joy_msg.buttons[2]==1)
-  {
+    {
     //msg.data="turn_left";
     //msg.data=cmd_list[2];
-    cmd_data.cmd_word=cmd_list[1];
-    for ( i = 0; i < 3; i++)
-    {
-    cmd_data.cmd_array[i]=list[1][i]
-    }
-    joy_pub.publish(msg);
-    ROS_INFO("left");
-  }
+      cmd_data.cmd_word=cmd_list[1];
+      for ( i = 0; i < 3; i++)
+      {
+      cmd_data.cmd_array[i]=list[1][i];
+      }
+      joy_pub.publish(msg);
+      ROS_INFO("left");
+      }
   if (joy_msg.buttons[3]==1)
-  {
+    {
     //msg.data="go_straight";
     //msg.data=cmd_list[0];
     cmd_data.cmd_word=cmd_list[1];
     for ( i = 0; i < 3; i++)
     {
-    cmd_data.cmd_array[i]=list[2][i]
+    cmd_data.cmd_array[i]=list[0][i];
     }
     joy_pub.publish(msg);
     ROS_INFO("go");
-  }
+    } 
 }
 
 uint64_t get_rand_range( uint64_t min_vel, uint64_t max_vel ){
@@ -77,29 +78,17 @@ uint64_t get_rand_range( uint64_t min_vel, uint64_t max_vel ){
 }
 
 int main(int argc, char **argv) {
-<<<<<<< HEAD
+
   ros::init(argc, argv, "joy_cmd");        // ノードの初期化（ノード名を"srv_test"に設定）
   ros::NodeHandle nh;                       // ノードハンドルの生成
   //joy_pub = nh.advertise<std_msgs::String>("joy_cmd", 10);
-  joy_pub = nh.advertise<joy_cmd::dir_cmd_msg>("cmd_data", 10);
-  
   joy_sub = nh.subscribe("joy", 10, Joy_Callback);
-  ROS_INFO("muripo");
-  while (1)
-  {
-    int p = get_rand_range(0,2);
-    std::cout << cmd_list[get_rand_range(0,2)] <<"\n";
-  }
-  
-  
-  ros::spin();                              // リクエストの無限待ち
-=======
-  ros::init(argc, argv, "joy_cmd");        
-  ros::NodeHandle nh;                       
-  joy_pub = nh.advertise<std_msgs::String>("joy_cmd", 10);
+  ROS_INFO("muripo");                
+  //joy_pub = nh.advertise<std_msgs::String>("joy_cmd", 10);
+  joy_pub = nh.advertise<joy_cmd::dir_cmd_msg>("cmd_data", 10);
   joy_sub = nh.subscribe("joy", 10, Joy_Callback);
   ROS_INFO("muripo");
   ros::spin();                              
->>>>>>> d3205cbfca409198b3909dac65c1ebccae8a3b3f
+
   return 0;
 }
