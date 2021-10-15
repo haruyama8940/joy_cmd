988f76af007f843ec43e4d94cefa9f41401b4635
haruyama8940 <yaruo0406@gmail.com>
2021-10-15 11:55:37 +0900

    fix

--- src/joy_cmd.cpp
+++ src/joy_cmd.cpp
@@ -5,16 +5,18 @@
 #include<array>
 #include <vector>
 #include <std_msgs/Int32.h>
+#include <std_msgs/Int8MultiArray.h>
 #include <std_msgs/String.h>
 #include <random>
 #include <iostream>
 
 ros::Subscriber joy_sub;
-ros::Publisher joy_pub;
+// ros::Publisher joy_pub;
+ros::Publisher cmd_data_pub;
 //boost::array<int,10>joy_b;
 std::vector<int> joy_b;
 sensor_msgs::Joy cmd_topic;
-
+std_msgs::Int8MultiArray cmd_data;
 std::vector<std::string> cmd_list = {"go_stright","turn_right","turn_left"};
 //std::array<int,3> data{0,0,0};
 int list[3][3]={
@@ -29,7 +31,7 @@ void Joy_Callback(const sensor_msgs::Joy& joy_msg)
   //cmd_topic = joy_msg;
   ROS_INFO("Joy_callback");
   //std_msgs::String msg;
-  joy_cmd::dir_cmd_msg cmd_data;
+  // joy_cmd::dir_cmd_msg cmd_data;
   /*cmd_data.cmd_word="go";
   //cmd_data.cmd_array[0]=1;
   //joy_pub.publish(cmd_data);
@@ -42,12 +44,13 @@ void Joy_Callback(const sensor_msgs::Joy& joy_msg)
   if (joy_msg.buttons[1]==1)
     {
       //msg.data="turn_right";
-      cmd_data.cmd_word=cmd_list[1];
+      // cmd_data.cmd_word=cmd_list[1];
       for ( i = 0; i < 3; i++)
       {
-      cmd_data.cmd_array[i]=list[1][i];
+      cmd_data.data[i]=list[1][i];
       }
-      joy_pub.publish(cmd_data);
+      // joy_pub.publish(cmd_data);
+      cmd_data_pub.publish(cmd_data);
       ROS_INFO("right");
     }
 
@@ -55,24 +58,27 @@ void Joy_Callback(const sensor_msgs::Joy& joy_msg)
     {
     //msg.data="turn_left";
     //msg.data=cmd_list[2];
-      cmd_data.cmd_word=cmd_list[2];
+      // cmd_data.cmd_word=cmd_list[2];
       for ( i = 0; i < 3; i++)
       {
-      cmd_data.cmd_array[i]=list[2][i];
+      cmd_data.data[i]=list[2][i];
       }
-      joy_pub.publish(cmd_data);
+      // joy_pub.publish(cmd_data);
+      cmd_data_pub.publish(cmd_data);
+
       ROS_INFO("left");
       }
   if (joy_msg.buttons[3]==1)
     {
     //msg.data="go_straight";
     //msg.data=cmd_list[0];
-    cmd_data.cmd_word=cmd_list[0];
+    // cmd_data.cmd_word=cmd_list[0];
     for ( i = 0; i < 3; i++)
     {
-    cmd_data.cmd_array[i]=list[0][i];
+    cmd_data.data[i]=list[0][i];
     }
-    joy_pub.publish(cmd_data);
+    // joy_pub.publish(cmd_data);
+    cmd_data_pub.publish(cmd_data);
     ROS_INFO("go");
     }
 }
@@ -91,7 +97,8 @@ int main(int argc, char **argv) {
   //joy_sub = nh.subscribe("joy", 10, Joy_Callback);
   ROS_INFO("muripo");                
   //joy_pub = nh.advertise<std_msgs::String>("joy_cmd", 10);
-  joy_pub = nh.advertise<joy_cmd::dir_cmd_msg>("cmd_data", 10);
+  // joy_pub = nh.advertise<joy_cmd::dir_cmd_msg>("cmd_data", 10);
+  cmd_data_pub =nh.advertise<std_msgs::Int8MultiArray>("cmd_data",10);
   joy_sub = nh.subscribe("joy", 10, Joy_Callback);
   ROS_INFO("muripo");
   ros::spin();                              
